0ff1f7a27048d1d1a438ac5fe732fb6143d59f9b
Devjonasbt <129806694+Devjonasbt@users.noreply.github.com>
2023-05-17 19:50:16 -0300

    Add files via upload

---
+++ verificadorAno/script.js
@@ -0,0 +1,28 @@
+function clicou(){
+
+    var data = new Date()
+    var anoAtual = data.getFullYear()
+
+    var ano = window.document.getElementById("ano")
+    var h = window.document.getElementById("hom")
+    var m = window.document.getElementById("mul")
+    var butao = window.document.getElementById("verificar")
+ 
+
+    if(ano.value.length == 1 || ano.value >anoAtual){
+        window.alert("[ERRO] verifica o ano e tente novamento")
+    }
+    else{
+        var sexF = document.getElementsByName('radio')
+        var idade = anoAtual - Number(ano.value)
+       
+        var genero = ""
+        if(sexF[0].checked){
+            genero = "Homem"
+        }
+        else if(sexF[1].checked){
+            genero = "Mulher"
+        }
+        butao.innerHTML = "Detectamos "+genero+" com "+idade+" anos"
+    }
+}
